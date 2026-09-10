5fe19e3588694cb307139c0dabb204a1c1bd9f9b
atamizh <atamil1207@gmail.com>
2026-09-10 13:00:41 +0530

    Added JS

---
+++ contact.js
@@ -0,0 +1,15 @@
+// Selecting Side Navbar,MenuIcon
+
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+
+menuicon.addEventListener ("click", function()
+{
+    sidenav.style.right=0
+})
+
+closenav.addEventListener("click",function()
+{
+    sidenav.style.right="-50%"
+})
+
